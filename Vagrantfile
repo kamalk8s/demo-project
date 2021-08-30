5735f8d5aa86515d90a37b5fe72511184fa090da
Kamal Kumar <Kamal.devops@outlook.com>
2021-08-30 15:47:16 +0530

    Added Vagrant File

---
+++ Vagrantfile
@@ -0,0 +1,23 @@
+Vagrant.configure("2") do |config|
+  config.vm.box = "ubuntu/focal64"
+
+  config.vm.network "forwarded_port", guest: 81, host: 81
+  config.vm.network "forwarded_port", guest: 8080, host: 8080
+  config.vm.network "forwarded_port", guest: 8081, host: 8081
+  config.vm.network "private_network", ip: "192.168.10.10"
+  # config.vm.synced_folder ".", "/app"
+
+  config.vm.provider "virtualbox" do |vb|
+    vb.memory = "4096"
+  end
+  
+  config.vm.provision "shell", inline: <<-SHELL
+    apt update && apt -y upgrade
+    apt install -y git
+    apt install python3.9
+    cd /
+    git clone https://github.com/kamalk8s/demo-project.git
+    cd demo-project
+    python3 /app/tools/install-requirements.py
+  SHELL
+end
--- tools/data-feeder.py
+++ tools/data-feeder.py
@@ -1,13 +1,13 @@
 import subprocess
 
 ## Feed the data
-subprocess.run(['sudo', 'chmod', '+x', './demo-api/data-feeder/data-feeder.sh'])
+subprocess.run(['sudo', 'chmod', '+x', './tools/data-feeder.sh'])
 if (subprocess.run(['http', '--version']).returncode != 0):
     installPython = subprocess.run(['sudo', 'apt', 'install', 'httpie', '-y'])
     if installPython.returncode == 0:
-        feeder = subprocess.run(['source', './demo-api/data-feeder/data-feeder.sh'])
+        feeder = subprocess.run(['source', './tools/data-feeder.sh'])
     else:
         print('Data Feed Failed. Make sure you have installed httpie')
 else:
-    feeder = subprocess.run(['/bin/bash', './demo-api/data-feeder/data-feeder.sh'])
-subprocess.run(['sudo', 'chmod', '-x', './demo-api/data-feeder/data-feeder.sh'])
+    feeder = subprocess.run(['/bin/bash', './tools/data-feeder.sh'])
+subprocess.run(['sudo', 'chmod', '-x', './tools/data-feeder.sh'])
